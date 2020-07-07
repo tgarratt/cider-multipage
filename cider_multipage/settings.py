b7f9520920901e800ee70272d4a3a84981ff1ec4
tgarratt <tomagarratt@yahoo.co.uk>
2020-07-07 12:50:38 +0000

    settings edit to modify for heroku

--- cider_multipage/settings.py
+++ cider_multipage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.DJANGOSECRETKEY
+SECRET_KEY = os.environ.get('DJANGOSECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,6 +91,7 @@ DATABASES = {
     }
 }
 
+#DATABASES = {'default': dj_database_url.parse(env.DATABASE_URL))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
