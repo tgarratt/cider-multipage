57e742b0faebd4c468d714826ba2f2d459136871
tgarratt <tomagarratt@yahoo.co.uk>
2020-07-27 12:07:36 +0000

    deploy mode to debug heroku

--- cider_multipage/settings.py
+++ cider_multipage/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-#import dj_database_url
+import dj_database_url
 try:
     import env
 except ModuleNotFoundError:
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 #FOR EDIT
-SECRET_KEY = env.DJANGOSECRETKEY
+#SECRET_KEY = env.DJANGOSECRETKEY
 #FOR DEPLOY
-#SECRET_KEY = os.environ.get('DJANGOSECRETKEY')
+SECRET_KEY = os.environ.get('DJANGOSECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -142,11 +142,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 #FOR EDIT
-STRIPE_PUBLISHABLE= env.STRIPE_PUBLISHABLE
-STRIPE_SECRET= env.STRIPE_SECRET
+#STRIPE_PUBLISHABLE= env.STRIPE_PUBLISHABLE
+#STRIPE_SECRET= env.STRIPE_SECRET
 #FOR DEPLOY
-#STRIPE_PUBLISHABLE= os.environ.get('STRIPE_PUBLISHABLE')
-#STRIPE_SECRET= os.environ.get('STRIPE_SECRET')
+STRIPE_PUBLISHABLE= os.environ.get('STRIPE_PUBLISHABLE')
+STRIPE_SECRET= os.environ.get('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
