7201fad7863948bb240be6e03e1e6f43ba9e88eb
tgarratt <tomagarratt@yahoo.co.uk>
2020-07-28 12:16:38 +0000

    added heroku to allowed hosts

--- cider_multipage/settings.py
+++ cider_multipage/settings.py
@@ -34,7 +34,9 @@ SECRET_KEY = os.environ.get('DJANGOSECRETKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'natura-cider.herokuapp.com'
+]
 
 
 # Application definition
