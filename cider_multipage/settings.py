d2a41220ea3482895379482b123628c105007d94
tgarratt <tomagarratt@yahoo.co.uk>
2020-08-01 13:32:42 +0000

    installed whitenoise to debug heroku

--- cider_multipage/settings.py
+++ cider_multipage/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cider_multipage.urls'
@@ -141,9 +142,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 #FOR EDIT
